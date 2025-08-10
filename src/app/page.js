5fdd3d2fa454d63ace72be6dcf1e435869a6a5b1
Gteck45 <gteck45@gmail.com>
2025-08-11 02:56:57 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
         </p>
         <div className="mt-6 flex gap-4 fade-in delay-2 items-center justify-center">
           <a
-            href="/resume.pdf"
+            href="https://me.princebidhuri.com/"
             className="px-6 py-2 bg-blue-600 hover:bg-blue-700 text-white rounded-xl shadow"
           >
             View Resume
